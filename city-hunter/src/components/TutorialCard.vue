3ea03188f29afec385af716d88c2fccf6a0e3a81
ESPRZ <siyuan.zhao.syesprz@gmail.com>
2024-11-05 16:30:33 -0500

    feat: basic hunt page

---
+++ city-hunter/src/components/TutorialCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from 'vue';
+import { mdiArrowRight } from '@mdi/js'
+import SvgIcon from '@jamescoyle/vue-icon';
+const props = defineProps({
+    title: String,
+    goNext: Function,
+});
+
+</script>
+
+<template>
+    <div class="tutorial-card">
+        <h2 class="tutorial-header">{{ title }}</h2>
+        <div class="tutorial-next">
+            <p>Let's Keey Going!</p>
+            <svg-icon @click=goNext type="mdi" :path="mdiArrowRight"></svg-icon>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+
+</style>
---
+++ city-hunter/src/views/HuntView.vue
@@ -0,0 +1,109 @@
+<script setup>
+import MapComponent from '../components/MapComponent.vue';
+import NavigationBar from '@/components/NavigationBar.vue'
+import RegisterCard from '@/components/RegisterCard.vue'
+import GeneralCard from '@/components/GeneralCard.vue';
+import { computed } from 'vue';
+import { useEventStore } from '@/stores/eventStore';
+import LocationNameCard from '@/components/LocationNameCard.vue';
+import DistanceCard from '@/components/DistanceCard.vue';
+import { useRouter } from 'vue-router';
+const eventStore = useEventStore();
+const isEnrolled = computed(() => eventStore.isEnrolled);
+const router = useRouter();
+function goToReward() {
+    router.push('/rewards');
+}
+function goToTodo() {
+    router.push('/todo');
+}
+</script>
+
+<template>
+  <div class="hunter-container">
+
+    <div class="loc-info">
+      <LocationNameCard title="XXX House"/>
+      <DistanceCard />
+
+    </div>
+
+    <div class="map-background">
+      <MapComponent />
+    </div>
+
+
+    <div class="hunt-info">
+      <div v-if="isEnrolled">
+        <div class="eventCard">
+          <GeneralCard title="To Do" if-magnify=true :magnify-action=goToTodo />
+          <GeneralCard title="Achieved" if-magnify=false :magnify-action=goToReward />
+        </div>
+      </div>
+      <div v-else>
+        <RegisterCard />
+      </div>
+    </div>
+
+    <NavigationBar class="navigation-bar" />
+
+  </div>
+</template>
+
+
+<style scoped>
+.map-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 90vh;
+  z-index: 2;
+}
+
+.loc-info {
+  position: absolute;
+  top: 0;
+
+  width: 100%;
+  height: auto;
+
+  background-color: transparent;
+
+  padding: 30px;
+
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: column;
+
+  gap: 20px;
+
+  justify-content: space-evenly;
+
+  z-index: 10;
+}
+
+.hunt-info {
+  position: absolute;
+  bottom: 10vh;
+  width: 100%;
+  height: auto;
+  background-color: var(--color-background);
+  padding: 30px;
+  border-radius: 30px 30px 0 0;
+  box-sizing: border-box;
+  z-index: 10;
+  overflow: hidden;
+}
+
+.eventCard {
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+}
+
+
+</style>
