a60e40e56592e5fc614717014890440058d9c0f2
ESPRZ <siyuan.zhao.syesprz@gmail.com>
2024-11-07 16:31:33 -0500

    feat: todo page

---
+++ city-hunter/src/components/TodoCard.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { defineProps } from 'vue';
+import { mdiWalk, mdiDirections } from '@mdi/js'
+import SvgIcon from '@jamescoyle/vue-icon';
+
+const props = defineProps({
+    title: String,
+    distance: Number,
+    time: Number
+});
+
+</script>
+
+<template>
+    <div class="todo-item-container">
+
+        <img class="shop-image" src="https://github.com/radix-vue.png" />
+
+        <div>
+            <h2>{{ title }}</h2>
+            <div class="distance">
+
+                <span class="distance-item">
+                    <svg-icon  type="mdi" :path=mdiDirections></svg-icon>
+                    <p>{{ distance }} km</p>
+                </span>
+
+                <span class="distance-item">
+                    <svg-icon type="mdi" :path=mdiWalk></svg-icon>
+                    <p>{{ time }} min</p>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.todo-item-container {
+    display: flex;
+}
+
+.shop-image {
+    width: 65px;
+    height: 65px;
+    margin-right: 20px;
+    border-radius: 100%;
+}
+
+.distance {
+    display: flex;
+    justify-content: start;
+    margin: 5px;
+    color: var(--gray);
+}
+
+.distance-item {
+    display: flex;
+    margin-right: 10px;
+}
+</style>
---
+++ city-hunter/src/views/TodoView.vue
@@ -0,0 +1,49 @@
+<script setup>
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiChevronDown, mdiExportVariant } from '@mdi/js';
+import TodoCard from '@/components/TodoCard.vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+function returnToHunt() {
+    router.push('/hunt');
+}
+const todoList = ref([
+    {
+        title: "XXX House",
+        distance: 1.23,
+        time: 12
+    },
+    {
+        title: "XXX House",
+        distance: 1.23,
+        time: 12
+    }
+])
+
+</script>
+
+<template>
+    <div class="page-container">
+        <div class="page-header">
+            <svg-icon @click=returnToHunt type="mdi" :path=mdiChevronDown></svg-icon>
+            <h1>To do</h1>
+            <svg-icon type="mdi" :path=mdiExportVariant></svg-icon>
+        </div>
+
+        <ul v-for="(todo, index) in todoList" :key="index" class="todo-item">
+            <TodoCard :title="todo.title" :distance="todo.distance" :time="todo.time" />
+        </ul>
+
+    </div>
+</template>
+
+<style scoped>
+.todo-item {
+    background-color: white;
+    margin: 20px;
+    padding: 13px;
+    border-radius: 10px;
+
+}
+</style>
