d029959b72416c95cac258929aee5ad2384b0417
ESPRZ <siyuan.zhao.syesprz@gmail.com>
2024-11-19 01:38:26 -0500

    feat: tutorial view

--- city-hunter/src/components/TutorialCard.vue
+++ city-hunter/src/components/TutorialCard.vue
@@ -1,26 +1,91 @@
 <script setup>
 import { defineProps } from 'vue';
-import { mdiArrowRight } from '@mdi/js'
+import { mdiArrowRightBold, mdiArrowDownBold } from '@mdi/js';
 import SvgIcon from '@jamescoyle/vue-icon';
+
 const props = defineProps({
     title: String,
     goNext: Function,
 });
-
 </script>
 
 <template>
     <div class="tutorial-card">
-        <h2 class="tutorial-header">{{ title }}</h2>
-        <div class="tutorial-next">
-            <p>Let's Keey Going!</p>
-            <svg-icon @click=goNext type="mdi" :path="mdiArrowRight"></svg-icon>
+        <div class="tutorial-text-container">
+            <h2>{{ title }}</h2>
+            <div class="tutorial-next">
+                <p>Let's Keep Going!</p>
+                <svg-icon @click="goNext" type="mdi" :path="mdiArrowRightBold" class="icon icon-right"></svg-icon>
+                <svg-icon @click="goNext" type="mdi" :path="mdiArrowDownBold" class="icon icon-down"></svg-icon>
+            </div>
         </div>
+        <img class="guide-cat" src="/guide_cat.svg" alt="Guide Cat" />
     </div>
 </template>
 
-
 <style scoped>
+.tutorial-card {
+    position: absolute;
+    top: 30%;
+    margin: auto 0;
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    z-index: 3;
+}
+
+.tutorial-text-container {
+    background-color: #f8d9e6;
+    padding: 20px;
+    border-radius: 20px 20px 0 20px;
+    z-index: 3;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    border-bottom: 5px solid var(--color2);
+}
 
 
+h2 {
+    line-height: 1.4;
+}
+
+.tutorial-next {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: var(--color2);
+    margin-top: 10px;
+}
+
+.tutorial-next p {
+    margin: 0;
+    font-size: 14px;
+    font-weight: bold;
+}
+
+
+.icon {
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+    color: var(--color-text);
+}
+
+.icon-down {
+    display: none;
+}
+
+.svg-icon:hover {
+    transform: scale(1.2);
+}
+
+
+.guide-cat {
+    position: absolute;
+    bottom: -150px;
+    right: -20px;
+    width: 100px;
+    z-index: 3;
+}
 </style>
--- city-hunter/src/views/TutorialView.vue
+++ city-hunter/src/views/TutorialView.vue
@@ -1,34 +1,65 @@
 <script setup>
 import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
-import MapComponent from '../components/MapComponent.vue';
 import TutorialCard from '../components/TutorialCard.vue';
 const router = useRouter();
 
-const steps = ref([
+const steps = [
   {
     title: 'Hi, I\'ll guide you through in this hunting journey',
+    top: '30%',
+    bg: "/tutorial/Tutorial 28.svg"
   },
   {
     title: 'This is where you are',
+    top: '55%',
+    bg: "/tutorial/Tutorial 29.svg"
   },
   {
-    title: 'Explore Your Surroundings and Discover Local Treasures!',
+    title: 'Explore and Discover Local Treasures!',
+    top: '60%',
+    bg: "/tutorial/Tutorial 30.svg"
+  },
+  {
+    title: 'Explore and Discover Local Treasures!',
+    top: '60%',
+    bg: "/tutorial/Tutorial 31.svg"
   },
   {
     title: 'Follow Our Suggested Route or Design Your Own Adventure!',
+    top: '55%',
+    bg: "/tutorial/Tutorial 32.svg"
   },
   {
-    title: 'Stay Updated on the Hunt Challenge Item List Right Here!',
+    title: 'Track Your Hunting Progress Here!',
+    top: '30%',
+    bg: "/tutorial/Tutorial 33.svg"
   },
-]);
+];
 
 const currentStepIndex = ref(0);
-const currentStep = computed(() => steps.value[currentStepIndex.value]);
+const currentStep = computed(() => steps[currentStepIndex.value]);
 
 function nextStep() {
-  if (currentStepIndex.value < steps.value.length - 1) {
+  if (currentStepIndex.value < steps.length - 1) {
     currentStepIndex.value++;
+    const tutorialElement = document.querySelector('.tutorial-card');
+    if (tutorialElement) {
+      tutorialElement.style.top = steps[currentStepIndex.value].top;
+    }
+    if (currentStepIndex.value == 2) {
+      setTimeout(() => {
+        currentStepIndex.value++;
+      }, 200);
+    }
+    if (currentStepIndex.value == steps.length - 1) {
+      const guideCat = document.querySelector('.guide-cat');
+      if (guideCat) guideCat.remove();
+      const arrowRight = document.querySelector('.icon-right');
+      if (arrowRight) arrowRight.remove();
+      const arrowDown = document.querySelector('.icon-down');
+      if (arrowDown) arrowDown.style.display = 'flex';
+    }
   } else {
     router.push('/hunt');
   }
@@ -39,7 +70,7 @@ function nextStep() {
 <template>
   <div class="tutorial">
     <div class="map-background">
-      <MapComponent :highlightRoad="currentStepIndex === 2" />
+      <img :src="currentStep.bg" />
     </div>
     <TutorialCard :title="currentStep.title" :goNext="nextStep" />
   </div>
@@ -52,6 +83,20 @@ function nextStep() {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+
+  img {
+    width: 100vw;
+  }
+}
+
+.map-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 1;
 }
 
 .map-background {
@@ -64,7 +109,4 @@ function nextStep() {
   height: 100%;
   z-index: 0;
 }
-
-
-
 </style>
